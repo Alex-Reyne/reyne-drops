f626413785edc8d395f7669820f0438678ce80a0
alex-reyne <creativereyne@gmail.com>
2022-07-13 04:31:58 -0600

    fix dev instagram link

--- pages/api/drops.js
+++ pages/api/drops.js
@@ -3,33 +3,33 @@
 export default function handler(req, res) {
   res.status(200).json({
     links: [
-      { id: 1, name: "Dev Portfolio", url: "https://alexreyne.me/" },
+      { id: 1, name: 'Dev Portfolio', url: 'https://alexreyne.me/' },
       {
         id: 2,
-        name: "Design Portfolio",
-        url: "https://creativereyne.ca/",
+        name: 'Design Portfolio',
+        url: 'https://creativereyne.ca/',
       },
-      { id: 3, name: "Github", url: "https://github.com/alex-reyne" },
+      { id: 3, name: 'Github', url: 'https://github.com/alex-reyne' },
       {
         id: 4,
-        name: "Linkedin",
-        url: "https://www.linkedin.com/in/alexanderreyne/",
+        name: 'Linkedin',
+        url: 'https://www.linkedin.com/in/alexanderreyne/',
       },
       {
         id: 5,
-        name: "My Resume",
-        url: "https://drive.google.com/file/d/1rvrY8h93EFONuJnC_6eURYJscZSnOp1V/view",
+        name: 'My Resume',
+        url: 'https://drive.google.com/file/d/1rvrY8h93EFONuJnC_6eURYJscZSnOp1V/view',
       },
       {
         id: 6,
-        name: "Dev/Design ig",
-        url: "https://www.instagram.com/AlexReyne.dev/",
+        name: 'Dev/Design ig',
+        url: 'https://www.instagram.com/AlexReyne.me/',
       },
       {
         id: 7,
-        name: "Photography ig",
-        url: "https://www.instagram.com/alexreyne.ph/",
+        name: 'Photography ig',
+        url: 'https://www.instagram.com/alexreyne.ph/',
       },
     ],
-  });
+  })
 }
