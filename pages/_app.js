f92501fad7839da3789f5c6f7ef4ab9f97071986
alex-reyne <creativereyne@gmail.com>
2022-07-14 21:35:49 -0600

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,20 @@ import { useEffect } from 'react/cjs/react.production.min'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', 'G-KJ9N40PKBC', {
+        page_path: url,
+      })
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
