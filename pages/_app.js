778141002a420f32848f853b1333c39dfc98d959
alex-reyne <creativereyne@gmail.com>
2022-07-14 22:05:33 -0600

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { useEffect } from 'react/cjs/react.production.min'
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
