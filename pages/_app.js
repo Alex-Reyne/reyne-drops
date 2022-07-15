fbbe1f3dfca65e1fbd3f9462fdd9bbc72c1f36a1
alex-reyne <creativereyne@gmail.com>
2022-07-14 21:36:47 -0600

    remove google analytics from main branch

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,6 @@
-import { useRouter } from 'next/router'
-import { useEffect } from 'react/cjs/react.production.min'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      window.gtag('config', 'G-KJ9N40PKBC', {
-        page_path: url,
-      })
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-
   return <Component {...pageProps} />
 }
 
