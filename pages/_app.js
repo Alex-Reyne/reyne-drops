100fc0ff7b97357eb96148b4d5e0b79ef805464e
alex-reyne <creativereyne@gmail.com>
2022-07-15 19:03:46 -0600

    fix: ???

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,22 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', 'G-KJ9N40PKBC', {
+        page_path: url,
+      })
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
