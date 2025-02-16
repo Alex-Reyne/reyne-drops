4b5b6b28ea05cf9e7acd61eab3d8de6daf101196
Alex-Reyne <creativereyne@gmail.com>
2025-02-16 11:36:47 -0700

    change to dynamic forced

--- pages/api/drops.js
+++ pages/api/drops.js
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export const dynamic = 'force-dynamic'
 
 export default function handler(req, res) {
   res.status(200).json({
